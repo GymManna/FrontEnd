2e0e8f893620748e3f43b888851fd99e4ccc53c8
yunit <yung9031@gmail.com>
2023-03-17 18:39:07 +0900

    [#23]로그인, 회원가입 페이지 BackEnd와 연동 로그인, 회원가입 연동 완료

--- src/views/auth/SignUp.vue
+++ src/views/auth/SignUp.vue
@@ -1,17 +1,54 @@
 <template>
   <div class="container">
-    <span> E-mail </span> <input type="text">
-    <span> 이름 </span> <input type="text">
-    <span> 닉네임 </span> <input type="text">
-    <span> Password </span> <input type="password">
-    <span> Password 확인 </span> <input type="password">
+    <span> E-mail </span> <input type="text" placeholder="E-mail 입력" v-model="userId">
+    <span> 이름 </span> <input type="text" placeholder="이름 입력" v-model="userName">
+    <span> 닉네임 </span> <input type="text" placeholder="닉네임 입력" v-model="userNickname">
+    <span> Password </span> <input type="password" placeholder="Password 입력" v-model="userPassword">
+    <span> 전화번호 </span> <input type="text" placeholder="전화번호 입력" v-model="userPhone">
   </div>
-    <button> 회원가입 </button>
+    <button @click="register()"> 회원가입 </button>
 </template>
 
 <script>
   export default {
-    name: 'SignUp'
+    name: 'SignUp',
+    data() {
+      return {
+        userId: "",
+        userName: "",
+        userNickname: "",
+        userPassword: "",
+        userPhone: "",
+      };
+    },
+    methods: {
+      register() {
+        console.log("@@ register() 실행");
+        var serverIP = process.env.VUE_APP_SERVER_IP,
+          serverPort = process.env.VUE_APP_SERVER_PORT,
+          pageUrl = "mygym/user/register";
+        this.$axios({
+          url: `http://${serverIP}:${serverPort}/${pageUrl}`,
+          method: "POST",
+          params: {
+            userId: this.userId,
+            userName: this.userName,
+            userNickname: this.userNickname,
+            userPassword: this.userPassword,
+            userPhone: this.userPhone,
+          },
+          responseType: "json",
+        })
+          .then((result) => {
+            console.log("axios 성공");
+            console.log(result);
+          })
+          .catch((error) => {
+            console.log("axios 실패");
+            console.log(error);
+          })
+      },
+    }
   }
 </script>
 
